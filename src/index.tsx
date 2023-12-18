8bb2709fb9ad266d6818adadeefe6fcaf085b01a
Sergey <serg9205@gmail.com>
2023-12-18 21:06:58 +0200

    feat: implement redux provider and redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
 import "./index.css";
 import { App } from "./App";
 import ReactDOM from "react-dom";
+import store from "./store/store";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
