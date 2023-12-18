3a5d660b2ac4ba87b7aadf1ed04418cf98f7396d
Sergey <serg9205@gmail.com>
2023-12-18 19:01:18 +0200

    feat: create basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,51 @@
 import React from "react";
 
 export const App: React.FC = () => {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <form>
+        <div>
+          <button>ТТН</button>
+          <button>Відділення</button>
+        </div>
+        <div>
+          <div>
+            <div>
+              <input type="text" />
+              <button>🔍</button>
+              <button>🗙</button>
+            </div>
+            <div>
+              <p>Статус:</p>
+              <p>Отримали:</p>
+              <p>Відправив:</p>
+            </div>
+          </div>
+          <div>
+            <div>
+              <p>Історія</p>
+              <button>🗑️</button>
+            </div>
+            <div>
+              <h1>TTN</h1>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <input type="text" />
+            <button>🗙</button>
+          </div>
+          <div>
+            <div>
+              <p>Cities</p>
+            </div>
+            <div>
+              <p>Warehouses</p>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
