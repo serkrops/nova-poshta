0c153ea9fa6157029e084d160930d5226c77c4e3
Sergey <serg9205@gmail.com>
2023-12-18 22:40:05 +0200

    fix: add placeholder to departmens input and history component to TTN component

--- src/components/Departments.tsx
+++ src/components/Departments.tsx
@@ -1,11 +1,24 @@
+import { useEffect, useRef } from "react";
 import CitiesList from "./CitiesList";
 import WarehousesList from "./WarehousesList";
+import { useSelector } from "react-redux";
+import { RootState } from "../types/types";
 
 const Departments: React.FC = () => {
+  const { isMailShow } = useSelector((state: RootState) => state.data);
+  const cityInputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (!isMailShow) {
+      cityInputRef.current?.focus();
+    }
+  }, [isMailShow]);
+
   return (
     <div className="flex flex-col gap-3 w-full h-80 md:max-h-[70vh]">
       <div className="flex gap-3 shadow-md shadow-gray-700/40 rounded-xl bg-slate-600/40 p-2 w-full">
         <input
+          ref={cityInputRef}
           type="text"
           className="bg-red-400/40 w-full shadow-lg shadow-red-700/30 rounded-xl p-3 text-white text-xl font-bold placeholder:text-white placeholder:text-xl focus:outline-0 focus:bg-red-500/60"
           placeholder="Введіть назву міста"
--- src/components/TTN.tsx
+++ src/components/TTN.tsx
@@ -1,12 +1,25 @@
+import { useEffect, useRef } from "react";
 import HistoryList from "./HistoryList";
 import TtnInfo from "./TtnInfo";
+import { useSelector } from "react-redux";
+import { RootState } from "../types/types";
 
 const TTN: React.FC = () => {
+  const { isMailShow } = useSelector((state: RootState) => state.data);
+  const ttnInputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (isMailShow) {
+      ttnInputRef.current?.focus();
+    }
+  }, [isMailShow]);
+
   return (
     <div className="flex flex-col gap-3 md:flex-row w-full md:max-h-[80vh]">
       <div className="flex flex-col gap-3 w-full">
         <div className="flex gap-2 p-2 w-full shadow-md shadow-gray-700/40 rounded-xl bg-slate-600/40">
           <input
+            ref={ttnInputRef}
             type="text"
             className="bg-red-400/40 w-full shadow-lg shadow-red-700/30 rounded-xl p-3 text-white text-xl font-bold placeholder:text-white placeholder:text-xl focus:outline-0 focus:bg-red-500/60"
             placeholder="Введіть номер ТТН"
